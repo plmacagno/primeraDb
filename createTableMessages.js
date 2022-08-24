938be5640b583da59036206a3214f9f75c68d1ed
Pablo Macagno <plmacagno@gmail.com>
2022-08-24 19:46:32 -0300

    primera base de datos

---
+++ createTableMessages.js
@@ -0,0 +1,17 @@
+const { optionsMariaDB } = require('./options/config.js');
+const knex = require('knex')(optionsMariaDB);
+
+exports.up = function(knex){
+knex.schema.createTable('messages', table => {
+	table.increments('id')
+	table.string('email')
+	table.string('message')
+	table.string('date')
+});
+};
+/*	.then(() => console.log('Table created'))
+	.catch(err => { console.log(err); throw err })
+	.finally(() => knex.destroy())*/
+	exports.down = function(knew){
+		return knex.schema.dropTable('messages');
+	};
